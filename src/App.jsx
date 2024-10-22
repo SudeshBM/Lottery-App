f6a41c299ce454aa95d925e291fd2592906bfc67
SudeshBM <subramanyasudesh@gmail.com>
2024-10-22 12:40:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Lottery from './Lottery';
+import Ticket from './Ticket';
+import {sum} from "./helper"
+
+function App() {
+  let winCondition = (ticket) => {
+    return ticket.every((num) => num === ticket[0]);
+  }
+  return (
+    <>
+    <Lottery n = {3} winCondition={winCondition}/>
+    </>
+  );
+}
+export default App
---
+++ src/helper.js
@@ -0,0 +1,11 @@
+function genTicket(n) {
+    let arr = new Array(n)
+    for (let i = 0; i < n; i++) {
+        arr[i] = Math.floor(Math.random() * 10) 
+    }
+    return arr
+}
+function sum(arr){
+    return arr.reduce((Sum,curr) => Sum + curr,0)
+}
+export{genTicket,sum};
